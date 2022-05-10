bc71cf100904cda42963917ec3d3956202562991
Jerry Zhang <jerryfish@163.com>
2022-05-10 15:51:32 +1200

    Create useLocalStorageState custom hook
    feat: create useLocalStorageState custom hook
    
    - Create useLocalStorageState custom hook to note down the ignoring condition in the local storage
    
    - Divide partially into two groups: notIgnored and ignored

--- client/src/components/InventoryInfo.js
+++ client/src/components/InventoryInfo.js
@@ -1,6 +1,9 @@
+import useLocalStorageState from '../utils/useLocalStorageState'
 import Product from './Product'
 
 export default function InventoryInfo({ localsOutOfStock }) {
+  const [ignoredProductIds, setIgnoredProductIds] = useLocalStorageState('ignoredProductIds', [])
+
   const { 
     activeLocallyOutOfStockProducts: gone, 
     activeLocallyHavingOutOfStockProducts: partially 
@@ -8,6 +11,15 @@ export default function InventoryInfo({ localsOutOfStock }) {
 
   if (!gone.length && !partially.length) {
     return <p>No products are out of stock locally.</p>
+  }
+
+  let notIgnored = []
+  let ignored = []
+
+  if (partially.length) {
+    partially.forEach(part => {
+      ignoredProductIds.indexOf(part.id) === -1 ? notIgnored.push(part) : ignored.push(part)
+    })
   }
 
   return (
@@ -21,12 +33,20 @@ export default function InventoryInfo({ localsOutOfStock }) {
         </div>
       )}
       {partially.length > 0 && (
-        <div style={{color: 'orange'}}>
-          <h3>Products having out of stock variants [Counts: {partially.length}]</h3>
-          {partially.map((p) => (
-            <Product key={p.id} product={p} fromInventory={true} isPartial={true} />
-          ))}
-        </div>
+        <>
+          <div style={{color: 'orange'}}>
+            <h3>Products having out of stock variants [Counts: {notIgnored.length}]</h3>
+            {notIgnored.map((p) => (
+              <Product key={p.id} product={p} fromInventory={true} isPartial={true} />
+            ))}
+          </div>
+          <div style={{color: 'purple'}}>
+            <h3>Products having out of stock variants [Counts: {ignored.length}]</h3>
+            {ignored.map((p) => (
+              <Product key={p.id} product={p} fromInventory={true} isPartial={true} />
+            ))}
+          </div>
+        </>
       )}
     </>
   )
