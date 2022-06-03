bfe8d0489d7a047279734918027a1ef82fc9898d
Jerry Zhang <jerryfish@163.com>
2022-06-03 17:31:31 +1200

    Add handling submit method
    feat: Add handling submit method

--- client/src/components/HiddenDetail.js
+++ client/src/components/HiddenDetail.js
@@ -1,7 +1,3 @@
-import { useRef, useState } from 'react'
-import useLocalStorageState from '../utils/useLocalStorageState'
-import Product from './Product'
-
 export default function HiddenDetail({
   productsWithHidden,
   settings: {
@@ -13,26 +9,45 @@ export default function HiddenDetail({
     mainTitle,
   },
 }) {
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const variantIds = []
+
+    for (let index = 0; index < e.target.length; index++) {
+      if (e.target[index].nodeName === 'INPUT' && e.target[index].checked)
+        variantIds.push(e.target[index].id)
+    }
+
+    console.log(variantIds)
+  }
+
   return (
     <div style={{ background: background }}>
       <h2>PRODUCTS WITH HIDDEN VARIANTS</h2>
 
       <div style={{ color: mainColor }}>
-        {productsWithHidden.map(product => (
-          <div key={product.id}>
-            <h3>{product.title}</h3>
-            <ul>
-              {product.variants.map(
-                variant =>
-                  variant.weight === 9999 && (
-                    <li
-                      key={variant.id}
-                    >{`${product.title} - ${variant.title}`}</li>
-                  )
-              )}
-            </ul>
-          </div>
-        ))}
+        <form onSubmit={handleSubmit}>
+          {productsWithHidden.map(product => (
+            <div key={product.id}>
+              <h3>{product.title}</h3>
+              <ul>
+                {product.variants.map(
+                  variant =>
+                    variant.weight === 9999 && (
+                      <li key={variant.id}>
+                        <input type="checkbox" id={variant.id} />
+                        <label
+                          htmlFor={variant.id}
+                        >{`${product.title} - ${variant.title}`}</label>
+                      </li>
+                    )
+                )}
+              </ul>
+            </div>
+          ))}
+          <button type="submit">REMOVE HIDDEN STATUS</button>
+        </form>
       </div>
     </div>
   )
