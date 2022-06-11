027d09daf72da73d695c498b81cc118db22a16e0
jerryfishzz <jerryfish@163.com>
2022-06-12 11:53:40 +1200

    Use BASE_REQUEST_URL
    refactor: Use BASE_REQUEST_URL in inventory and variant apis to reduce code repeating

--- src/shopifyAPIs/inventory.js
+++ src/shopifyAPIs/inventory.js
@@ -1,8 +1,13 @@
 import fetch from 'node-fetch'
-import { API_VERSION, options, STORE_NAME } from "../utils/shopifyConfig.js"
+import { options, BASE_REQUEST_URL } from '../utils/shopifyConfig.js'
+
+const INVENTORY_REQUEST_URL = `${BASE_REQUEST_URL}inventory_levels.json`
 
 export const queryByItemAndLocation = async (inventoryItemIds, locationIds) => {
-  const response = await fetch(`https://${STORE_NAME}.myshopify.com/admin/api/${API_VERSION}/inventory_levels.json?inventory_item_ids=${inventoryItemIds}&location_ids=${locationIds}`, options)
+  const response = await fetch(
+    `${INVENTORY_REQUEST_URL}?inventory_item_ids=${inventoryItemIds}&location_ids=${locationIds}`,
+    options
+  )
 
   return response.json()
 }
--- src/shopifyAPIs/variant.js
+++ src/shopifyAPIs/variant.js
@@ -1,27 +1,24 @@
 import fetch from 'node-fetch'
-import { API_VERSION, options, STORE_NAME } from '../utils/shopifyConfig.js'
+import { options, BASE_REQUEST_URL } from '../utils/shopifyConfig.js'
+
+const createVariantRequestUrl = variantId =>
+  `${BASE_REQUEST_URL}variants/${variantId}.json`
 
 export const queryByVariantId = async variantId => {
-  const response = await fetch(
-    `https://${STORE_NAME}.myshopify.com/admin/api/${API_VERSION}/variants/${variantId}.json`,
-    options
-  )
+  const response = await fetch(createVariantRequestUrl(variantId), options)
 
   return response.json()
 }
 
 export const updateByVariantId = async (variantId, data) => {
-  const response = await fetch(
-    `https://${STORE_NAME}.myshopify.com/admin/api/${API_VERSION}/variants/${variantId}.json`,
-    {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': process.env.API_SECRET_KEY,
-      },
-      body: JSON.stringify(data),
-    }
-  )
+  const response = await fetch(createVariantRequestUrl(variantId), {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': process.env.API_SECRET_KEY,
+    },
+    body: JSON.stringify(data),
+  })
 
   return response.json()
 }
