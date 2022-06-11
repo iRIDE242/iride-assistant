549432cf803b21bcd5400a66c2c1ebedd84a0183
jerryfishzz <jerryfish@163.com>
2022-06-12 10:54:40 +1200

    Add try catch block
    feat: add try catch block for all the async apis

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,20 +2,32 @@ export const getVariantLocationInventory = async (
   locationIds,
   inventoryItemIds
 ) => {
-  const response = await fetch(
-    `/inventory?location_ids=${locationIds}&inventory_item_ids=${inventoryItemIds}`
-  )
-  return response.json()
+  try {
+    const response = await fetch(
+      `/inventory?location_ids=${locationIds}&inventory_item_ids=${inventoryItemIds}`
+    )
+    return response.json()
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 export const getVariantById = async variantId => {
-  const response = await fetch(`/variant?variant_id=${variantId}`)
-  return response.json()
+  try {
+    const response = await fetch(`/variant?variant_id=${variantId}`)
+    return response.json()
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 export const resetVariantWeightById = async variantId => {
-  const response = await fetch(`/variant/weight?variant_id=${variantId}`)
-  return response.json()
+  try {
+    const response = await fetch(`/variant/weight?variant_id=${variantId}`)
+    return response.json()
+  } catch (error) {
+    return Promise.reject(error)
+  }
 }
 
 export const getProductsByCollectionId = async collectionId => {
