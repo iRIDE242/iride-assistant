d2a2558aca5c520ea1f222004bceaccb43313a05
jerryfishzz <jerryfish@163.com>
2022-06-13 21:55:20 +1200

    Modify queryProducts
    refactor: Modify queryProducts by using handle fetch function

--- src/shopifyAPIs/products.js
+++ src/shopifyAPIs/products.js
@@ -1,12 +1,10 @@
-import fetch from 'node-fetch'
 import { handleFetch } from '../utils/functions.js'
 import { OPTIONS_GET, BASE_REQUEST_URL } from '../utils/shopifyConfig.js'
 
 const PRODUCT_REQUEST_URL = `${BASE_REQUEST_URL}products.json`
 
-export const queryProducts = async () => {
-  const response = await fetch(PRODUCT_REQUEST_URL, OPTIONS_GET)
-  return response.json()
+export const queryProducts = () => {
+  return handleFetch(PRODUCT_REQUEST_URL, OPTIONS_GET)
 }
 
 export const queryByCollectionId = collectionId => {
