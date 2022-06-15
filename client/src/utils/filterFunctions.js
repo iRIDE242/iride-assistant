b92ede1cfaf15108a39b492753efdd7a3bafdf0d
jerryfishzz <jerryfish@163.com>
2022-06-15 19:35:16 +1200

    Correct the way to write ramda filter in map
    bug: Correct the way to write ramda filter in map

--- client/src/utils/filterFunctions.js
+++ client/src/utils/filterFunctions.js
@@ -10,10 +10,18 @@ const hasHidden = product => {
 
 const getAllFilters = filterFuncs => {
   return filterFuncs.length > 0
-    ? filterFuncs.length > 1
-      ? pipe(...filterFuncs.map(filter))
-      : filter(filterFuncs[0])
+    // Note, in map, the ramda 'filter' actually will return a function that receive an array as paramter to be invoked.
+    // It is equal to arr => arr.filter(func) shown below.
+    // So it cannot be written as 'filterFuncs.map(filter)'.
+    ? pipe(...filterFuncs.map(filterFunc => filter(filterFunc)))
     : filter(filterAll)
 }
+
+// This is the way not using ramda filter method but only js
+// const getAllFilters = filterFuncs => {
+//   return filterFuncs.length > 0
+//     ? pipe(...filterFuncs.map(func => arr => arr.filter(func)))
+//     : filter(filterAll)
+// }
 
 export { filterAll, hasHidden, getAllFilters }
