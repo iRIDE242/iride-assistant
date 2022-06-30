0fe14912c935ac41c1fd94402c9f325f0cff24f8
Jerry Zhang <jerryfish@163.com>
2022-06-30 17:27:54 +1200

    Migrate getLocallyOutOfStockProducts
    refactor: Migrate getLocallyOutOfStockProducts
    
    - Create products in action
    
    - Migrate getLocallyOutOfStockProducts from product to products
    
    - Modify the import path in App

--- client/src/actions/product.js
+++ client/src/actions/product.js
@@ -1,11 +1,9 @@
-import { equals, filter, pipe, prop, reduce } from 'ramda'
+import { equals, filter, pipe, prop } from 'ramda'
 import {
   getProductById,
-  getVariantLocationInventory,
   resetVariantWeightById,
 } from '../utils/api'
-import { LOCAL_LOCATION_ID } from '../utils/config'
-import { getInventoryItemId, isHidden } from './variantAPIs'
+import { isHidden } from './variantAPIs'
 import { createSequencedPromises } from '../utils/helper'
 
 /**
@@ -23,88 +21,6 @@ export const isActive = pipe(getStatus, equals('active'))
 /**
  * Async requests
  */
-
-/**
- * Get products that are active and locally out of stock
- */
-
-// Create promise relay
-const createLocallyNonHiddenInventoryRelayByVariant = variant => {
-  if (isHidden(variant)) return false
-
-  const inventoryItemId = getInventoryItemId(variant)
-
-  return async (res, rej) => {
-    try {
-      const {
-        inventory: { inventory_levels },
-      } = await getVariantLocationInventory(LOCAL_LOCATION_ID, inventoryItemId)
-      const { available } = inventory_levels[0]
-
-      res(available)
-    } catch (error) {
-      rej(error)
-    }
-  }
-}
-
-// Use relay to create all the variants' promises to check if local non-hidden variants are out of stock
-const areLocalNonHiddenOutOfStock = async product => {
-  let status = 'in stock'
-
-  const promiseContainer = createSequencedPromises(
-    getVariants(product),
-    createLocallyNonHiddenInventoryRelayByVariant
-  )
-
-  try {
-    const inventories = await Promise.all(promiseContainer)
-
-    const processAdd = (acc, cur) => {
-      if (cur <= 0 && status === 'in stock')
-        status = 'has variants out of stock'
-      return acc + cur
-    }
-
-    const totalNonHiddensInventory = reduce(processAdd, 0)(inventories)
-    // Note, reduce from Ramda needs to give the initial value
-
-    console.log(`${product.title}: ${totalNonHiddensInventory}`)
-
-    if (totalNonHiddensInventory <= 0) status = 'out of stock'
-    console.log(status)
-
-    return status
-  } catch (error) {
-    throw error
-  }
-}
-
-// Get the results related to locally out of stock products
-export const getLocallyOutOfStockProducts = async products => {
-  const activeProducts = filter(isActive)(products)
-
-  const activeLocallyOutOfStockProducts = []
-  const activeLocallyHavingOutOfStockProducts = []
-
-  try {
-    for (let index = 0; index < activeProducts.length; index++) {
-      const status = await areLocalNonHiddenOutOfStock(activeProducts[index])
-
-      if (status === 'out of stock')
-        activeLocallyOutOfStockProducts.push(activeProducts[index])
-      if (status === 'has variants out of stock')
-        activeLocallyHavingOutOfStockProducts.push(activeProducts[index])
-    }
-
-    return {
-      activeLocallyOutOfStockProducts,
-      activeLocallyHavingOutOfStockProducts,
-    }
-  } catch (error) {
-    throw error
-  }
-}
 
 /**
  * Check if there are hidden variants
---
+++ client/src/actions/products.js
@@ -0,0 +1,92 @@
+import { equals, filter, pipe, prop, reduce } from 'ramda'
+import { getInventoryItemId, isHidden } from './variantAPIs'
+import {
+  getProductById,
+  getVariantLocationInventory,
+  resetVariantWeightById,
+} from '../utils/api'
+import { LOCAL_LOCATION_ID } from '../utils/config'
+import { createSequencedPromises } from '../utils/helper'
+import { getVariants, isActive } from './product'
+
+/**
+ * Get products that are active, and completely or partially out of stock locally
+ */
+
+// Create promise relay
+const createLocallyNonHiddenInventoryRelayByVariant = variant => {
+  if (isHidden(variant)) return false
+
+  const inventoryItemId = getInventoryItemId(variant)
+
+  return async (res, rej) => {
+    try {
+      const {
+        inventory: { inventory_levels },
+      } = await getVariantLocationInventory(LOCAL_LOCATION_ID, inventoryItemId)
+      const { available } = inventory_levels[0]
+
+      res(available)
+    } catch (error) {
+      rej(error)
+    }
+  }
+}
+
+// Use relay to create all the variants' promises to check if local non-hidden variants are out of stock
+const areLocalNonHiddenOutOfStock = async product => {
+  let status = 'in stock'
+
+  const promiseContainer = createSequencedPromises(
+    getVariants(product),
+    createLocallyNonHiddenInventoryRelayByVariant
+  )
+
+  try {
+    const inventories = await Promise.all(promiseContainer)
+
+    const processAdd = (acc, cur) => {
+      if (cur <= 0 && status === 'in stock')
+        status = 'has variants out of stock'
+      return acc + cur
+    }
+
+    const totalNonHiddensInventory = reduce(processAdd, 0)(inventories)
+    // Note, reduce from Ramda needs to give the initial value
+
+    console.log(`${product.title}: ${totalNonHiddensInventory}`)
+
+    if (totalNonHiddensInventory <= 0) status = 'out of stock'
+    console.log(status)
+
+    return status
+  } catch (error) {
+    throw error
+  }
+}
+
+// Get the results related to completely or partially locally out of stock products
+export const getLocallyOutOfStockProducts = async products => {
+  const activeProducts = filter(isActive)(products)
+
+  const activeLocallyOutOfStockProducts = []
+  const activeLocallyHavingOutOfStockProducts = []
+
+  try {
+    for (let index = 0; index < activeProducts.length; index++) {
+      const status = await areLocalNonHiddenOutOfStock(activeProducts[index])
+
+      if (status === 'out of stock')
+        activeLocallyOutOfStockProducts.push(activeProducts[index])
+      if (status === 'has variants out of stock')
+        activeLocallyHavingOutOfStockProducts.push(activeProducts[index])
+    }
+
+    return {
+      activeLocallyOutOfStockProducts,
+      activeLocallyHavingOutOfStockProducts,
+    }
+  } catch (error) {
+    throw error
+  }
+}
