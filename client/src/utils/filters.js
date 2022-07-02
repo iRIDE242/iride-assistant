f3a221a0e4d73319e487eef178baeccbd131c267
jerryfishzz <jerryfish@163.com>
2022-07-02 14:57:09 +1200

    Use bypass to replace filterAll
    feat: Use bypass to replace filterAll
    
    - Create bypass to not filter anything and use it as default method when dealing with product filters

--- client/src/utils/filters.js
+++ client/src/utils/filters.js
@@ -1,7 +1,7 @@
 import { filter, pipe, prop } from 'ramda'
 import { getHiddens } from '../actions/product'
 
-export const filterAll = () => false
+const bypass = any => any
 
 export const hasHidden = product => {
   const hiddens = getHiddens(product)
@@ -21,12 +21,12 @@ export const getAllFilters = filters => {
       // It is equal to arr => arr.filter(func) shown below.
       // So it cannot be written as 'filterFuncs.map(filter)'.
       pipe(...filterFuncs.map(filterFunc => filter(filterFunc)))
-    : filter(filterAll)
+    : bypass
 }
 
 // This is the way not using ramda filter method but only js
 // const getAllFilters = filterFuncs => {
 //   return filterFuncs.length > 0
 //     ? pipe(...filterFuncs.map(func => arr => arr.filter(func)))
-//     : filter(filterAll)
+//     : bypass
 // }
