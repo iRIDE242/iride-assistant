eb4a121e71af773f2b229f0fd1d83e024cf4a07a
Jerry Zhang <jerryfish@163.com>
2022-07-21 11:27:08 +1200

    Update useCheckboxHost.js
    chore: add comments for useCheckboxHost

--- client/src/custom-hooks/useCheckboxHost.js
+++ client/src/custom-hooks/useCheckboxHost.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { updateParentCheckbox } from '../utils/helper'
 
+// For checkbox which state is defined outside of component
+// since its state is also used by other components.
 export const useCheckboxHost = (checkedFromParent, setParentCheckbox) => {
   const [checkboxHost, setCheckboxHost] = useState({
     checked: false,
@@ -16,14 +18,14 @@ export const useCheckboxHost = (checkedFromParent, setParentCheckbox) => {
 
   // Synced with parent checkbox state
   useEffect(() => {
-    console.log('host - toggle child checkebox by parent')
+    console.log('host - toggle child checkebox from parent')
     setCheckboxHost({
       checked: checkedFromParent,
       fromParent: true,
     })
   }, [checkedFromParent])
 
-  // Manipulate the indeterminate state of section and product checkboxes
+  // Update selected state in parent checkbox
   useEffect(() => {
     updateParentCheckbox(
       checkboxHost.checked,
