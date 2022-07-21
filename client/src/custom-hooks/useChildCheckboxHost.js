52d14a11e80c2c8a5f5514394e683518e0f2e89e
Jerry Zhang <jerryfish@163.com>
2022-07-21 12:34:54 +1200

    Add support for grandparent checkbox
    feat: Add support for grandparent checkbox in useChildCheckboxHost

--- client/src/custom-hooks/useChildCheckboxHost.js
+++ client/src/custom-hooks/useChildCheckboxHost.js
@@ -3,36 +3,66 @@ import { updateParentCheckbox } from '../utils/helper'
 
 // For child checkbox which state is defined outside of component
 // since its state is also used by other components.
-export const useChildCheckboxHost = (checkedFromParent, setParentCheckbox) => {
+export const useChildCheckboxHost = (
+  checkedFromParent,
+  setParentCheckbox,
+  setGrandParentCheckbox,
+  fromSection
+) => {
   const [childCheckboxHost, setChildCheckboxHost] = useState({
     checked: false,
     fromParent: true,
+    fromSection: null,
   })
 
   const handleCheckboxHostChange = () => {
     setChildCheckboxHost(current => ({
       checked: !current.checked,
       fromParent: false,
+      fromSection: current.fromSection === null ? null : false,
     }))
   }
 
   // Synced with parent checkbox state
   useEffect(() => {
     console.log('host - toggle child checkebox from parent')
-    setChildCheckboxHost({
+    setChildCheckboxHost(current => ({
       checked: checkedFromParent,
       fromParent: true,
-    })
-  }, [checkedFromParent])
+      fromSection:
+        fromSection === undefined ? current.fromSection : fromSection,
+    }))
+  }, [checkedFromParent, fromSection])
 
-  // Update selected state in parent checkbox
+  // Update selected state in parent checkbox for indeterminate value.
+  // Only responde to the checked change caused by the component itself,
+  // but not checked change caused by the parent checkbox sync.
   useEffect(() => {
     updateParentCheckbox(
       childCheckboxHost.checked,
       childCheckboxHost.fromParent,
       setParentCheckbox
     )
-  }, [childCheckboxHost.checked, childCheckboxHost.fromParent, setParentCheckbox])
+  }, [
+    childCheckboxHost.checked,
+    childCheckboxHost.fromParent,
+    setParentCheckbox,
+  ])
+
+  // Update selected state in grandparent checkbox for indeterminate value.
+  useEffect(() => {
+    setGrandParentCheckbox &&
+      childCheckboxHost.fromSection !== null &&
+      updateParentCheckbox(
+        childCheckboxHost.checked,
+        childCheckboxHost.fromSection,
+        setGrandParentCheckbox
+      )
+  }, [
+    childCheckboxHost.checked,
+    childCheckboxHost.fromSection,
+    setGrandParentCheckbox,
+  ])
 
   return [childCheckboxHost, handleCheckboxHostChange]
 }
