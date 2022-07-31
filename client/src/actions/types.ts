7bb0226654dd1e809e890e969fe1598d7bb173ff
jerryfishzz <jerryfish@163.com>
2022-07-31 22:09:02 +1200

    Add InventoryLevel type
    feat: Add InventoryLevel type

--- client/src/actions/types.ts
+++ client/src/actions/types.ts
@@ -8,3 +8,11 @@ export enum InventoryStatus {
   PARTIALLY_OUT = 'has variants out of stock',
   OUT_OF_STOCK = 'out of stock',
 }
+
+export interface InventoryLevel {
+  inventory_item_id: number
+  location_id: number
+  available: number
+  updated_at: Date
+  admin_graphql_api_id: string
+}
