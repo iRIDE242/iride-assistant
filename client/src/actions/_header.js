6b4787cd6df2be2edcdfb61995499de37efba2d3
jerryfishzz <jerryfish@163.com>
2022-08-01 12:58:13 +1200

    Migrate header action to ts
    refactor: Migrate header action to ts
    
    - Migrate header action to ts; add related types

--- client/src/actions/_header.js
+++ client/src/actions/_header.js
@@ -2,6 +2,10 @@ import { pipe, prop } from 'ramda'
 
 const regexGeneratorForLink = direction => {
   const base = `<https:\\/\\/[\\w\\d-]+\\.myshopify\\.com\\/admin\\/api\\/[\\d]{4}-[\\d]{2}\\/products\\.json\\?limit=(?<limit>[\\d]+)&page_info=(?<pageInfo>[\\w\\d]+)>;\\srel="${direction}"`
+  // Named capture groups
+  // If matched, the result will be an array of matched string and matched groups,
+  // plus a propery on the array 'groups' with matched groups as its member.
+
   return new RegExp(base, 'i')
 }
 
@@ -15,7 +19,7 @@ const getNextMatched = link => regexNext.exec(link)
 
 const getPageLink = matchedResult => {
   if (matchedResult === null) return { limit: '', pageInfo: '' }
-
+console.log(matchedResult)
   const {
     groups: { limit, pageInfo },
   } = matchedResult
@@ -24,6 +28,7 @@ const getPageLink = matchedResult => {
 
 // Get prev and next page info from the response header
 export const createPrevAndNextFromHeader = header => {
+  console.log(JSON.stringify(header))
   return {
     prev: pipe(getLink, getPrevMatched, getPageLink)(header),
     next: pipe(getLink, getNextMatched, getPageLink)(header),
--- client/src/actions/types.ts
+++ client/src/actions/types.ts
@@ -16,3 +16,57 @@ export interface InventoryLevel {
   updated_at: Date
   admin_graphql_api_id: string
 }
+
+export enum Directions {
+  NEXT = 'next',
+  PREVIOUS = 'previous',
+}
+
+// Regex named capture groups
+// https://github.com/microsoft/TypeScript/issues/32098#issuecomment-922114332
+type MatchedHeaderWithGroupsOnly<T> = {
+  groups: {
+    [key in keyof T]: string
+  }
+}
+
+export type MatchedResult<T> =
+  | (RegExpExecArray & MatchedHeaderWithGroupsOnly<T>)
+  | null
+
+export interface Header {
+  'alt-svc': string
+  'cf-cache-status': string
+  'cf-ray': string
+  connection: string
+  'content-encoding': string
+  'content-security-policy': string
+  'content-type': string
+  date: string
+  'expect-ct': string
+  http_x_shopify_shop_api_call_limit: string
+  link: string
+  'referrer-policy': string
+  server: string
+  'strict-transport-security': string
+  'transfer-encoding': string
+  vary: string
+  'x-content-type-options': string
+  'x-dc': string
+  'x-download-options': string
+  'x-frame-options': string
+  'x-permitted-cross-domain-policies': string
+  'x-request-id': string
+  'x-shardid': string
+  'x-shopid': string
+  'x-shopify-api-terms': string
+  'x-shopify-api-version': string
+  'x-shopify-shop-api-call-limit': string
+  'x-shopify-stage': string
+  'x-sorting-hat-podid': string
+  'x-sorting-hat-shopid': string
+  'x-stats-apiclientid': string
+  'x-stats-apipermissionid': string
+  'x-stats-userid': string
+  'x-xss-protection': string
+}
